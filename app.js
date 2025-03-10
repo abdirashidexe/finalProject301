76bf040af84d9f6fb3ee4e441ff2f40574c1c0a2
unknown <174657090+Butenkite@users.noreply.github.com>
2025-03-10 11:36:22 -0700

    home connection to database

--- app.js
+++ app.js
@@ -1,69 +1,71 @@
-import express from 'express';
-import mariadb from 'mariadb';
-import { validateForm } from './services/validation.js';
-import dotenv from 'dotenv';
+import express from "express";
+import mariadb from "mariadb";
+import { validateForm } from "./services/validation.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 //Define our database credentials
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 //Define function to connect to the DB
 async function connect() {
-    try {
-        const conn = await pool.getConnection();
-        console.log('Connected to the database!');
-        return conn;
-    } catch (err) {
-        console.log(`Error connecting to the database ${err}`);
-    }
+  try {
+    const conn = await pool.getConnection();
+    console.log("Connected to the database!");
+    return conn;
+  } catch (err) {
+    console.log(`Error connecting to the database ${err}`);
+  }
 }
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const PORT = process.env.APP_PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", async (req, res) => {
+  const conn = await connect();
+  const parts = await conn.query("SELECT * FROM parts");
+  res.render("home", { parts });
 });
 
-app.post('/loginUser', (req, res) => {
-    res.render('home');
+app.post("/loginUser", (req, res) => {
+  res.render("home");
 });
 
-app.get('/admin', async (req, res) => {
-    const conn = await connect();
-    const orders = await conn.query('SELECT * FROM shopping_cart');
-    res.render('admin', { orders });
+app.get("/admin", async (req, res) => {
+  const conn = await connect();
+  const orders = await conn.query("SELECT * FROM shopping_cart");
+  res.render("admin", { orders });
 });
 
-app.get('/confirmed', (req, res) => {
-    res.render('confirmed');
+app.get("/confirmed", (req, res) => {
+  res.render("confirmed");
 });
 
-app.get('/Advanced_Filters', (req, res) => {
-    res.render('filters');
-}); 
+app.get("/Advanced_Filters", (req, res) => {
+  res.render("filters");
+});
 
-app.get('/cart', async (req, res) => {
-    const conn = await connect();
-    const shoppingCart = await conn.query('SELECT * FROM shopping_cart');
-    res.render('cart', { shoppingCart});
+app.get("/cart", async (req, res) => {
+  const conn = await connect();
+  const shoppingCart = await conn.query("SELECT * FROM shopping_cart");
+  res.render("cart", { shoppingCart });
 });
 
 //Tell the server to listen on our specified port
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
