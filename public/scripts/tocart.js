ca5ed9b18e3461c74ee8d067dfcffa93469b4770
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-03-11 08:31:43 -0700

    cart popup added

--- public/scripts/tocart.js
+++ public/scripts/tocart.js
@@ -1,4 +1,12 @@
-const addToCartButtons = document.getElementsByClassName("addToCartBtn");
+
+const addToCartButtons = document.querySelectorAll(".addToCartBtn")
+
+const popup = document.getElementById('popup');
+const closeBtn = document.getElementById('closePopup');
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
 
 const selectedParts = [];
 // this is just so that we can press nothing and view cart. to view cart, we need two inputs.
@@ -10,16 +18,19 @@ sessionStorage.setItem("selectedParts", JSON.stringify(selectedParts));
 // All the 'add to cart' buttons grab the id their parents are associated with
 // and then give that id to the selectedParts array.
 // its sick af when I dont use dumb ids and use the actual part id amen baby
-for (let i = 0; i < addToCartButtons.length; i++) {
-  addToCartButtons[i].addEventListener("click", function (event) {
-    const partId = this.parentElement.id;
-    if (!selectedParts.includes(partId)) {
-      selectedParts.push(partId);
-      sessionStorage.setItem("selectedParts", JSON.stringify(selectedParts));
-    }
-    console.log(selectedParts);
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', function () {
+      popup.style.display = 'flex';
+      
+      const partId = this.parentElement.id;
+      if (!selectedParts.includes(partId)) {
+          selectedParts.push(partId);
+          sessionStorage.setItem("selectedParts", JSON.stringify(selectedParts));
+      }
+      console.log(selectedParts);
   });
-}
+});
+
 
 // This sends all the buttons we pressed to the server. I couldn't
 // figure out how to send the data to the server individually. I clump
