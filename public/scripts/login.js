9b46e43a7de11dd6b09a3c7b1811c3f043b0ffc2
unknown <174657090+Butenkite@users.noreply.github.com>
2025-03-19 15:46:28 -0700

    sign up basic

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,13 +1,17 @@
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btnOpen = document.getElementById("myBtn");
+
+var signUp = document.getElementById("signUpChange");
 
 var modal = document.getElementById("myModal");
+
+//  Primary stuff for span!!!
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
+btnOpen.onclick = function () {
   document.getElementById("myModal").style.display = "block";
 };
 
@@ -21,4 +25,11 @@ window.onclick = function (event) {
   if (event.target == modal) {
     document.getElementById("myModal").style.display = "none";
   }
+};
+
+// sign up stuff!!!
+
+signUp.onclick = function () {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("signUp").style.display = "block";
 };
